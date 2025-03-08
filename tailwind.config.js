b3cd03075efb91f97f07a92e382a4c27170cf776
Natha Lopes Zerbo <nathalopeszerbo7@gmail.com>
2025-03-08 00:27:21 -0300

    Atualizando

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-        colors: {
-        'hoverspt': '#18D760',
-        'purple-main': '#2D46B9',
-        'green-main': '#1ED760'
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
